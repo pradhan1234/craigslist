37a49f2945b3921156b4357070cf82b9453d115d
pradhan1234 <=>
2018-03-17 15:13:56 -0500

    Adding initial files for the WPL Project

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+
+	echo "You are on registration.php";
+	echo "<br>Name : ".$name;
+	echo "<br>Email : ".$email;
+	echo "<br>Password : "."*****";
+?>
